cf59f5eb5e3e8a2fa0f0eda63f557d05c3943c58
Leon Jäkel <info@leon-jaekel.com>
2018-02-25 23:53:21 +0100

    ionic list app added

---
+++ ionicList/src/pages/home/home.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  items: Array<string> = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  onItemSelect(item: string) {
+    console.log("Selected Item", item);
+  };
+
+}
