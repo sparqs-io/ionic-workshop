275dac94ca4098af91c30dce0728f212bc75b85b
Leon Jäkel <info@leon-jaekel.com>
2018-02-26 00:24:00 +0100

    complex list app prepared

--- ionicComplexList/src/pages/home/home.ts
+++ ionicComplexList/src/pages/home/home.ts
@@ -7,6 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  items: Array<{
+    name: string;
+    avatarUrl: string;
+  }> = [
+    {
+      name: "Venkman",
+      avatarUrl: "assets/imgs/avatars/venkman.jpg"
+    },
+    {
+      name: "Egon",
+      avatarUrl: "assets/imgs/avatars/spengler.jpg"
+    },
+    {
+      name: "Egon",
+      avatarUrl: "assets/imgs/avatars/stantz.jpg"
+    },
+    {
+      name: "Winston",
+      avatarUrl: "assets/imgs/avatars/winston.jpg"
+    },
+    {
+      name: "Tully",
+      avatarUrl: "assets/imgs/avatars/tully.jpg"
+    },
+    {
+      name: "Dana",
+      avatarUrl: "assets/imgs/avatars/barrett.jpg"
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
